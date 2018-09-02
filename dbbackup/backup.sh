287425154a61c802f896d54e5b04c1638cc533a1
Dávid Balatoni <balcsida@gmail.com>
2018-09-02 21:10:11 +0200

    Add dbbackup

---
+++ dbbackup/backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "Job started: $(date)"
+
+DATE=$(date +%Y%m%d_%H%M%S)
+FILE="/backups/backup-$DATE.tar.gz"
+
+mongodump -h $MONGO_HOST --db rocketchat --archive $FILE --gzip
+
+echo "Job finished: $(date)"
