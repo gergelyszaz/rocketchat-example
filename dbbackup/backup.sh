eb18dd316a06d1108c2a5bb7eb010bb80279e0eb
Dávid Balatoni <balcsida@gmail.com>
2018-10-01 20:44:53 +0200

    Fix archive

--- dbbackup/backup.sh
+++ dbbackup/backup.sh
@@ -7,6 +7,6 @@ echo "Job started: $(date)"
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/backups/backup-$DATE.tar.gz"
 
-mongodump -h $MONGO_HOST --db rocketchat --archive --out $FILE --gzip
+mongodump --host $MONGO_HOST --db rocketchat --archive=$FILE --gzip
 
 echo "Job finished: $(date)"
