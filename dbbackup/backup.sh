5e0332fb74e4699831d03de586207f314eb0757f
Dávid Balatoni <balcsida@gmail.com>
2018-10-06 17:21:00 +0200

    Working backup state with 7z compression

--- dbbackup/backup.sh
+++ dbbackup/backup.sh
@@ -5,8 +5,11 @@ set -e
 echo "Job started: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
-FILE="/backups/backup-$DATE.tar.gz"
+FILE="/backups/backup-$DATE.7z"
 
-mongodump --host $MONGO_HOST --db rocketchat --archive=$FILE --gzip
+mkdir $DATE
+mongodump --host $MONGO_HOST --db rocketchat -o $DATE
+7z a -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on $FILE $DATE
+rm -rf $DATE
 
 echo "Job finished: $(date)"
--- dbbackup/healthcheck.sh
+++ dbbackup/healthcheck.sh
@@ -2,6 +2,4 @@
 
 if [ $(mongo --host $MONGO_HOST:$MONGO_PORT --eval 'db.getMongo().getDBNames().indexOf("rocketchat")' --quiet) -lt 0 ]; then
     echo "Database rocketchat does not exist"
-else
-    echo "Database rocketchat exists"
 fi
