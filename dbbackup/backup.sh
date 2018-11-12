d3b2fc1380c8dbe232668a71708771e6a07c6e70
balcsida <balcsida@gmail.com>
2018-11-12 19:08:09 +0100

    Refine 7z arguments to prevent its murder

--- dbbackup/backup.sh
+++ dbbackup/backup.sh
@@ -9,7 +9,7 @@ FILE="/backups/backup-$DATE.7z"
 
 mkdir $DATE
 mongodump --host $MONGO_HOST --db rocketchat -o $DATE
-7z a -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on $FILE $DATE
+7z a -m0=lzma2 -mx=8 $FILE $DATE
 rm -rf $DATE
 
 echo "Job finished: $(date)"
