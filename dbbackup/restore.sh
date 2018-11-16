ed2dd3d413ae4543d7d385213fc1f8f08010b33b
Gergely Száz <gergely.szaz@liferay.com>
2018-11-16 13:51:49 +0100

    LRSUPPORT-26192 Use relateive paths

--- dbbackup/restore.sh
+++ dbbackup/restore.sh
@@ -15,17 +15,17 @@ else
 fi
 
 # get latest backup
-FILE="backups/$(ls backups | grep "backup-.*\.7z" | tail -1)"
+FILE="/backups/$(ls /backups | grep "backup-.*\.7z" | tail -1)"
 echo "Latest database backup file found: $FILE"
 
 # extract
-if [ -n "$(ls -A dump/)" ]; then
-	echo "dump/ folder is not empty, clearing contents"
-	rm -rd dump/*
+if [ -n "$(ls -A /dump/)" ]; then
+	echo "/dump/ folder is not empty, clearing contents"
+	rm -rd /dump/*
 fi
 
 echo "Extracting $FILE to dump/"
-7za x "$FILE" -odump
+7za x "$FILE" -o/dump
 
 # restore DB
 echo "Running mongorestore"
